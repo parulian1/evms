1720f6db1c813d9be8b8d41f7153b4524e8ef276
martogi.sitanggang <martogi@apps-foundry.com>
2025-02-07 22:39:55 +0700

    add ref_name so api docs (drf_yasg) can atleast work, need to find out how href can be shown at api docs

--- api/utils/helpers.py
+++ api/utils/helpers.py
@@ -2,22 +2,21 @@ import re
 from http import HTTPStatus
 from typing import Union
 
+
 from rest_framework import exceptions, serializers
 from rest_framework.views import Response, exception_handler
 
 
 http_code_to_message = {v.value: v.description for v in HTTPStatus}
 
-
 def get_entity_href_serializer(model_class, meta_extra_kwargs=None, *init_args, **init_kwargs):
     class EntityHrefSerializer(serializers.HyperlinkedModelSerializer):
         name = serializers.CharField(required=False)
-
         class Meta:
             model = model_class
             fields = ('href', 'name',)
             extra_kwargs = meta_extra_kwargs if meta_extra_kwargs is not None else {'href': {'lookup_field': 'id', }, }
-
+            ref_name = f'{model_class.__name__}HrefSerializer'
     return EntityHrefSerializer(*init_args, **init_kwargs)
 
 
